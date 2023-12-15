d5f06828df56785ba23009302877ece4c3c02f94
Kri Wis <dober@Kris-MacBook-Pro.local>
2023-12-15 15:12:39 +0700

    add route

--- account/go.mod
+++ account/go.mod
@@ -1,4 +1,4 @@
-module github.com/krittawtcode/memrizr-v2
+module github.com/krittawatcode/memrizr-v2
 
 go 1.21.5
 
--- account/main.go
+++ account/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/krittawatcode/memrizr-v2/handler"
 )
 
 func main() {
@@ -17,10 +18,8 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/api/account", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "kindly folks",
-		})
+	handler.NewHandler(&handler.Config{
+		R: router,
 	})
 
 	srv := &http.Server{

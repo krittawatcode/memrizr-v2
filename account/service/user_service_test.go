2b25a1027892ac418d6117e66b81c79366d3ef76
Kri Wis <dober@Kris-MacBook-Pro.local>
2024-01-04 16:05:19 +0700

    add service layer with testing

---
+++ account/service/user_service_test.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/krittawatcode/memrizr-v2/model"
+	"github.com/krittawatcode/memrizr-v2/model/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGet(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserResp := &model.User{
+			UID:   uid,
+			Email: "bob@bob.com",
+			Name:  "Bobby Bobson",
+		}
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(&USConfig{
+			UserRepository: mockUserRepository,
+		})
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(mockUserResp, nil)
+
+		// just mocking the ctx for now
+		ctx := context.TODO()
+		// calling the user service method that we are testing
+		u, err := us.Get(ctx, uid)
+
+		assert.NoError(t, err)
+		assert.Equal(t, u, mockUserResp)
+		mockUserRepository.AssertExpectations(t)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		uid, _ := uuid.NewRandom()
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		us := NewUserService(&USConfig{
+			UserRepository: mockUserRepository,
+		})
+
+		mockUserRepository.On("FindByID", mock.Anything, uid).Return(nil, fmt.Errorf("Some error down the call chain"))
+
+		ctx := context.TODO()
+		u, err := us.Get(ctx, uid)
+
+		assert.Nil(t, u)
+		assert.Error(t, err)
+		mockUserRepository.AssertExpectations(t)
+	})
+}
